693e4afec0f7e37b5d2e518aacbfbf7f7ac0d507
anantkshirsagar <40562120+anantkshirsagar@users.noreply.github.com>
2019-05-14 22:39:42 +0530

    Admin Project first commit

---
+++ src/com/fileupload/UploadServlet.java
@@ -0,0 +1,40 @@
+package com.fileupload;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileUploadException;
+
+import com.fileupload.util.FileUploadUtils;
+
+/**
+ * Servlet implementation class UploadServlet
+ */
+@WebServlet("/UploadServlet")
+public class UploadServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	public static final String uploadFilePath = "H:\\eclipse-project-workspace\\AdminProject\\uploadedfiles";
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		try {
+			//FileUploadUtils.uploadFile(uploadFilePath, request);
+			Map<String, InputStream> fileInputStream = FileUploadUtils.getFileInputStream(request);
+			for(Map.Entry<String, InputStream> fileIS : fileInputStream.entrySet()) {
+				System.out.println(" FileName: " +fileIS.getKey());
+				FileUploadUtils.writeStream(fileIS.getValue(), new File(uploadFilePath, fileIS.getKey()));
+			}
+		} catch (FileUploadException e) {
+			System.out.println(" Fileupload exception: " +e);
+		}
+	}
+}
